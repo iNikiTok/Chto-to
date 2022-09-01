276fd18f7b751e54a8414180f54c5723f658c370
Nikita <k0956493763@gmail.com>
2022-09-01 17:38:14 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
-//Здача 1
-a = prompt("Введите число ")
+//Задача 1
+/*a = prompt("Введите число ")
+if (a == 0) {
+    console.log("Verno")
+}else{
+    console.log("Ne verno")
+}*/
+//Задача 2
+/*a = prompt("Введите число ")
+if (a > 0) {
+    console.log("Verno")
+}else{
+    console.log("Ne verno")
+}*/
+// Задача 3
+/*a = prompt("Введите число ")
+if (a < 0) {
+    console.log("Verno")
+}else{
+    console.log("Ne verno")
+}*/
+// Задача 4
+/*a = prompt("Введите число ")
+if (a >= 0) {
+    console.log("Verno")
+}else{
+    console.log("Ne verno")
+}*/
+// Задача 5
+/*a = prompt("Введите число ")
+if (a <= 0) {
+    console.log("Verno")
+}else{
+    console.log("Ne verno")
+}*/
